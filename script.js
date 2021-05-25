9ac8a8699c4d2f858304ca8559ad7875fad91634
nasrulf <75235863+nasrulf@users.noreply.github.com>
2021-05-25 14:57:03 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function compute()
+{
+    p = document.getElementById("principal").value;
+    var principal = parseFloat(document.getElementById("principal").value);    
+    var rate = parseFloat(document.getElementById("rate").value);
+    var years = parseInt(document.getElementById("years").value);
+    var interest = principal * years * rate /100;
+    var yearInTheFuture = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark>,<br/>" +
+        "at an interest rate of <mark>" + rate + "%</mark>,<br/>" +
+        "You will receive an amount of <mark>" + interest + "</mark>,<br/> " +
+        "in the year <mark>" + yearInTheFuture + "</mark>";   
+}
+
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+function validateAmount() {
+    var principal = document.getElementById("principal").value;
+    var biggerThanZero = parseInt(principal) > 0;
+    if (!biggerThanZero) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+
+}
+
+  
